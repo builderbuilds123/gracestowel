4f60c83cb1ceae74359c22f63d24e258ca609998
Leon <leonshichuan@gmail.com>
2025-12-06 00:02:41 -0800

    add s3

--- apps/backend/medusa-config.ts
+++ apps/backend/medusa-config.ts
@@ -33,15 +33,23 @@ module.exports = defineConfig({
   },
   modules: [
     {
-      resolve: "@medusajs/file-s3",
+      resolve: "@medusajs/file",
       options: {
-        s3_url: process.env.S3_URL,
-        bucket: process.env.S3_BUCKET,
-        region: process.env.S3_REGION,
-        access_key_id: process.env.S3_ACCESS_KEY_ID,
-        secret_access_key: process.env.S3_SECRET_ACCESS_KEY,
-        cache_control: process.env.S3_CACHE_CONTROL || "public, max-age=31536000",
-        download_file_duration: 60 * 60, // 1 hour
+        providers: [
+          {
+            resolve: "@medusajs/file-s3",
+            id: "s3",
+            options: {
+              s3_url: process.env.S3_URL,
+              bucket: process.env.S3_BUCKET,
+              region: process.env.S3_REGION,
+              access_key_id: process.env.S3_ACCESS_KEY_ID,
+              secret_access_key: process.env.S3_SECRET_ACCESS_KEY,
+              cache_control: process.env.S3_CACHE_CONTROL || "public, max-age=31536000",
+              download_file_duration: 60 * 60, // 1 hour
+            },
+          },
+        ],
       },
     },
     {
